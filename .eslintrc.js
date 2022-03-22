23e6c567bdbd43266441ccd84116d13c21ae9969
Connie Leung <cleung334@gmail.com>
2022-03-22 23:16:42 +0800

    chore(deps): install eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript'
   ],
   root: true,
   env: {
@@ -21,5 +23,16 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'prettier/prettier': ['error', {"singleQuote": true, "trailingComma": "all", "printWidth": 120, "tabWidth": 2, "semi": false}],
+  },
+  "settings": {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      "typescript": {
+        "alwaysTryTypes": true // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
+      "project": ".",
+    }
   },
 };

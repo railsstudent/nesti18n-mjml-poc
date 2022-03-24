7d0591415b28b12a7b150684e8cd5b583747c3e7
Connie Leung <cleung334@gmail.com>
2022-03-25 01:05:28 +0800

    refactor: translation

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -33,28 +33,17 @@ export class UserService {
 
   private async renderWelcomeEmail(lang: string, welcomeUser: WelcomeUser) {
     const { organization, name, total, email } = welcomeUser
-    const title = await this.i18nService.translate('email.welcomeUser.TITLE', {
-      args: { name },
-      lang,
-    })
 
-    const welcomeText = await this.i18nService.translate('email.welcomeUser.WELCOME', {
-      args: { organization },
+    const title = await this.translate('email.welcomeUser.TITLE', { name }, lang)
+    const welcomeText = await this.translate('email.welcomeUser.WELCOME', { organization }, lang)
+    const membershipFee = await this.translate(
+      'email.welcomeUser.MEMBERSHIP_FEE',
+      { total, startDate: new Date(Date.now()).toISOString() },
       lang,
-    })
-
-    const membershipFee = await this.i18nService.translate('email.welcomeUser.MEMBERSHIP_FEE', {
-      args: { total, startDate: new Date(Date.now()).toISOString() },
-      lang,
-    })
-
-    const thankYouText = await this.i18nService.translate('email.welcomeUser.THANKS_AND_BEST_REGARDS', {
-      lang,
-    })
-
-    const admin = await this.i18nService.translate('email.welcomeUser.ADMIN', {
-      lang,
-    })
+    )
+    const thankYouText = await this.translate('email.welcomeUser.THANKS_AND_BEST_REGARDS', {}, lang)
+    const admin = await this.translate('email.welcomeUser.ADMIN', {}, lang)
+    const subject = await this.translate('email.welcomeUser.SUBJECT', {}, lang)
 
     const html = this.mjmlService.renderMjml('welcome-user.mjml', {
       title,
@@ -65,12 +54,20 @@ export class UserService {
       admin,
     })
 
-    const subject = await this.i18nService.translate('email.welcomeUser.SUBJECT', { lang })
-
     await this.mailerService.send({
       to: email,
       subject,
       html,
     })
+  }
+
+  private translate(key: string, args: Record<string, any>, lang: string) {
+    if (Object.keys(args).length > 0) {
+      return this.i18nService.translate(key, {
+        args,
+        lang,
+      })
+    }
+    return this.i18nService.translate(key, { lang })
   }
 }

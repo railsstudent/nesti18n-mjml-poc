5b4217fcfa32305788b009f6d7afc6437426cc22
Connie Leung <cleung334@gmail.com>
2022-03-24 15:17:27 +0800

    chore: eslintrc
    chore: eslintrc
    
    chore: eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,9 +30,9 @@ module.exports = {
     },
     "import/resolver": {
       "typescript": {
-        "alwaysTryTypes": true // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        "alwaysTryTypes": true,
+        "project": "."
       },
-      "project": ".",
     }
   },
 };
